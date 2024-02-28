3e22d33314d82af03c326d8c3660a5ebfbf74c67
bibadfd <151863975+bibadfd@users.noreply.github.com>
2024-02-28 19:33:26 +0200

    Add files via upload

---
+++ Схема1.js
@@ -0,0 +1,17 @@
+function compress(a) {
+    a = unescape(encodeURIComponent(a));
+    GID("im2").src = "https://www.plantuml.com/plantuml/check/" + encode64(zip_deflate(a, 9))
+}
+
+function changed() {
+    var b = GID("im2");
+    var a = b.src.replace("/check/", "/png/");
+    if (b.width == 1) {
+        GID("im").src = a;
+        GID("err").style.visibility = "hidden";
+        GID("err").src = "soft/shema/pixel.png"
+    } else {
+        GID("err").src = a;
+        GID("err").style.visibility = "visible"
+    }
+};
